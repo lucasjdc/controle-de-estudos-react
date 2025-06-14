3854532bcc586e4490f5b5e95d283126ed55ff33
lucasjdc <lucasjpdc@gmail.com>
2025-06-14 16:49:08 -0300

    Estilizando com Sass

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,3 +1,5 @@
+import './style.scss';
+
 function Lista() {
     const tarefas = [{
         tarefa: "React",
@@ -10,11 +12,11 @@ function Lista() {
         tempo: "03:00:00"
     }]
     return (
-        <aside>
+        <aside className="listaTarefas">
             <h2>Estudos do dia</h2>
                 <ul>
                     {tarefas.map((item, index) => (
-                        <li key={index}>
+                        <li key={index} className="item">
                             <h3>{item.tarefa}</h3>
                             <span>{item.tempo}</span>
                         </li>
