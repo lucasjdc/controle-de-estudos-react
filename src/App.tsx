8ab93de65bc29db1a727a223d885ae217e65fa5c
lucasjdc <lucasjpdc@gmail.com>
2025-06-14 13:56:55 -0300

    Criando um componente

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Botao from "./components/Botao";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <Botao/>
+        
+        
+    
+      </div>
+      
+    </>
+  )
+}
+
+export default App
